63b6206bbd3ce8f1c8737c728a077e41d97fc4e5
kalinin <prozaik81-2@yandex.ru>
2017-06-30 22:27:10 +0300

    ajax ok

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,13 @@ $(function() {
 	});
 });
 
-function submitDatesForm() {
+function getStatistic(unixDatesObj) {
+
+};
+
+function getUnixDatesObj() {
+	//console.log('getUnixDatesObj');
+
 	var dateBegin = $('#datepickerBegin').val(), 
 		dateEnd = $('#datepickerEnd').val();
 
@@ -13,18 +19,80 @@ function submitDatesForm() {
 		return;
 	}
 
-	// преобразуем в js-формат
-	var dateBeginObj = new Date(dateBegin.split('-')[0], dateBegin.split('-')[1], dateBegin.split('-')[2]);
-	var dateEndObj = new Date(dateEnd.split('-')[0], dateEnd.split('-')[1], dateEnd.split('-')[2]);
+    var yearBegin = dateBegin.split('-')[0],
+      monthBegin = +dateBegin.split('-')[1] - 1,
+      dayBegin = dateBegin.split('-')[2]
 
-	// преобразуем в unix-формат
-	var dateBeginUnix = dateBeginObj.getTime() / 1000;
-	var dateEndUnix = dateEndObj.getTime() / 1000;
+    if(monthBegin < 10)  { monthBegin = '0' + monthBegin }
 
-	console.log(dateBeginUnix, dateEndUnix);
+    var yearEnd = dateEnd.split('-')[0],
+      monthEnd = +dateEnd.split('-')[1] - 1,
+      dayEnd = dateEnd.split('-')[2]
 
-	/*return {
+    if(monthEnd < 10)  { monthEnd = '0' + monthEnd }
+
+    // преобразуем в js-формат
+    var dateBeginObj = new Date(yearBegin, monthBegin, dayBegin);
+    var dateEndObj = new Date(yearEnd, monthEnd, dayEnd);
+
+    // преобразуем в unix-формат
+    var dateBeginUnix = dateBeginObj.getTime() / 1000;
+    var dateEndUnix = dateEndObj.getTime() / 1000;
+
+	return {
 		'dateBeginUnix': dateBeginUnix,
 		'dateEndUnix': dateEndUnix
-	}*/
+	}
+};
+
+function constructTable(statistic) {
+	var tableBodyElem;
+
+	statistic.forEach(function(item) {
+	  if(item) {
+	    tableBodyElem += '<tr>'; 
+	    tableBodyElem += '<td>' + item.fields.created_date.split('T')[0] + '</td>'; 
+	    tableBodyElem += '<td>' + item.fields.training_time_min + '</td>'; 
+	    tableBodyElem += '<td>' + item.fields.pulse_avg_bpm + '</td>'; 
+	    tableBodyElem += '<td>' + item.fields.pulse_max_bpm + '</td>'; 
+	    tableBodyElem += '</tr>'; 
+	  }
+	});
+
+	var tableElem = '<table class="statistic-table" border="1" celpadding="8"> \
+	          <thead>\
+	            <tr>\
+	              <td>Дата</td>\
+	              <td>Время тренировки</td>\
+	              <td>Сердний пульс</td>\
+	              <td>Максимальный пульс</td>\
+	            </tr>\
+	          </thead>\
+	          <tbody>\
+	            <tr id=tableBody>' + tableBodyElem + '</tr>\
+	          </tbody>\
+	        </table>';
+
+	return tableElem;
+};
+
+function displayStatistic() {
+	var	unixDatesObj = getUnixDatesObj();
+	console.log('unixDatesObj', unixDatesObj);
+
+	$.ajax({
+		dataType: 'json',
+		type: 'GET',
+		url: "http://127.0.0.1:8000/days/" + unixDatesObj.dateBeginUnix + "/" + unixDatesObj.dateEndUnix, 
+		success: function(data) {
+			var statistic = JSON.parse(data);
+
+			var table = constructTable(statistic);
+
+			$('#statisticTableWrap').append(table);		
+		},
+		error: function(xhr, status, error) {
+			console.log(xhr, status, error);		
+		}
+	});
 };
